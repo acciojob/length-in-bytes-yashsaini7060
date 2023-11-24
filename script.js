ff2ae08e766c5fca9a8ab95b66527c5a47c20d90
Yash Kumar <85291913+yashsaini7060@users.noreply.github.com>
2023-11-25 00:05:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 const byteSize = (str) => {
   // write your code here
+ const blob = new Blob([str]);
+
+  // Retrieve the size property of the Blob object
+  const sizeInBytes = blob.size;
+
+  // Return the size in bytes
+  return sizeInBytes;
+
 };
 
 // Do not change the code below
